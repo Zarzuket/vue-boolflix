3a9113f2810d4cb86a49e0218f01e2c7c12a7c44
Zarzuket <davide.bajo@gmail.com>
2021-10-07 17:25:23 +0200

    adding flip card

--- src/components/Serie.vue
+++ src/components/Serie.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="card">
-        <div class="info">
-            <img :src="`http://image.tmdb.org/t/p/w342/${serie.poster_path}`" alt="Image Not Found">
-            <div>
+    <div class="flip-card">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <img :src="`http://image.tmdb.org/t/p/w342/${serie.poster_path}`" alt="Image Not Found">
+            </div>
+            <div class="flip-card-back">
                 <h4 class="title"><span>Titolo :</span>{{serie.name}}</h4>
                 <h4><span>Titolo originale:</span> {{serie.original_name}}</h4>
                 <h4 v-if="serie.original_language == 'it'"><span>Lingua:</span><img class="flag_img" src="../assets/img/it.svg" alt=""></h4>
@@ -12,6 +14,7 @@
             </div>
         </div>
     </div>
+
 </template>
 
 <script>
@@ -20,11 +23,6 @@
 export default {
     name:"Serie",
     props: ["serie"],
-data(){
-    return{
-        stars:[]
-    }
-},
 methods:{
     realVote(){
         return Math.ceil(this.serie.vote_average / 2);
@@ -38,22 +36,49 @@ span{
     color: red;
     font-size: 20px;
 }
-.card{
-    border: 1px solid green;
+.flip-card {
+    background-color: transparent;
     margin: 1.875rem;
     width: calc((100% / 4) - 15rem);
-    .info{
-        height: 100%;
-        width: 100%;
-        .flag_img{
-            width: 15%;
-        }
-        img{
-            width: 100%;
-            
-        }
-    };
 }
 
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+  .flag_img{
+      width: 15%;
+  }
+  img{
+      width: 100%;
+  }
+}
 
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+/* Position the front and back side */
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+
+/* Style the back side */
+.flip-card-back {
+  background-color: dodgerblue;
+  color: black;
+  transform: rotateY(180deg);
+}
 </style>
